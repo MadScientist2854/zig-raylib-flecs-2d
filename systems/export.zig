c0771f73256d87340d89f3f603d6885a4e1b45a8
MadScientist2854 <madscientist2854@gmail.com>
2021-08-17 16:38:32 +0500

    draw_text needs position component

--- systems/export.zig
+++ systems/export.zig
@@ -3,5 +3,5 @@ const flecs = @import("flecs");
 pub usingnamespace @import("draw_text.zig");
 
 pub fn init(world: *flecs.World) void {
-    _ = world.newSystem("Draw Text", .on_store, "Text", draw_text);
+    _ = world.newSystem("Draw Text", .on_store, "Text, Position2D", draw_text);
 }
